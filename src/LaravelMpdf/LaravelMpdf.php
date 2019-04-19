e14fd87310e752d82bbddfb8e8a044a5f304267b
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2019-04-19 11:24:40 +0600

    [bug-fix] config issues

--- src/LaravelMpdf/LaravelMpdf.php
+++ src/LaravelMpdf/LaravelMpdf.php
@@ -42,7 +42,7 @@ class LaravelMpdf {
 				$this->getConfig('custom_font_dir')
 			]),
 			'fontdata' => $fontData + $this->getConfig('custom_font_data'),
-			'default_font' => $this->getConfig('default_font')
+			'default_font' => $this->getConfig('default_font'),
 			'autoScriptToLang' => $this->getConfig('auto_language_detection'), 
 			'autoLangToFont' => $this->getConfig('auto_language_detection')
 		]);
